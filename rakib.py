57c7fd370682d6815166dbff03017cc26bc0311f
srakib009 <67092201+srakib009@users.noreply.github.com>
2020-06-18 13:05:47 +0600

    Update rakib.py

--- rakib.py
+++ rakib.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•MD - RAKIB -HOSSAIN------•◈•
+|__\~~) •||•RAKIB - KING---------------•◈•
+|__(-----\  •◈•------KING-RAKIB--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRAKIB
+\033[1;96mInstagram \033[1;93m: \033[1;FlowRAKIB
+\033[1;96mFacebook  \033[1;93m: \033[1; rakiib.rs
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/srakib009/rakib009
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRakib Hossain
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRakib
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Rakib Hossain
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/srakib009/rakib009
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "rakib"
+CorrectPassword = "rakib009"
 
 loop = 'true'
 while (loop == 'true'):
